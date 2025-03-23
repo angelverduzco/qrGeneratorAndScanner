86f27a4f2b3657dfedc8f0b3954d24cb20d2bbf6
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2025-03-23 14:40:46 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'qrGenerator',
+  webDir: 'dist'
+};
+
+export default config;
